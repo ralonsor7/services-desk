d7c5fea25f9ff48a8f74a5f2416699e7766a9e37
Rubén Alonso Ramiro <ruben.rar@hotmail.com>
2025-02-12 19:22:29 +0000

    proyecto desplegado

---
+++ app/assets/dist/Component-preload.js
@@ -0,0 +1,5 @@
+//@ui5-bundle servicesdesk/Component-preload.js
+sap.ui.require.preload({
+	"servicesdesk/manifest.json":'{"_version":"1.49.0","sap.app":{"id":"servicesdesk","applicationVersion":{"version":"1.0.0"},"type":"application","title":"","description":"servicesdesk","i18n":"i18n/i18n.properties","crossNavigation":{"inbounds":{"servicesdesk.tipossolicitud-display":{"semanticObject":"servicesdesk.tipossolicitud","action":"display","signature":{"parameters":{},"additionalParameters":"allowed"}}}}},"sap.cloud":{"public":true,"service":"servicesdesk.service"}}'
+});
+//# sourceMappingURL=Component-preload.js.map
