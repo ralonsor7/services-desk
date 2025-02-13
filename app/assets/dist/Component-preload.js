a7ebc324dc8cf89bb3b02f97e2d1db596b380b07
Rubén Alonso Ramiro <ruben.rar@hotmail.com>
2025-02-13 09:10:16 +0000

    moficiaciones semanticObjects

--- app/assets/dist/Component-preload.js
+++ app/assets/dist/Component-preload.js
@@ -1,5 +1,5 @@
 //@ui5-bundle servicesdesk/Component-preload.js
 sap.ui.require.preload({
-	"servicesdesk/manifest.json":'{"_version":"1.49.0","sap.app":{"id":"servicesdesk","applicationVersion":{"version":"1.0.0"},"type":"application","title":"","description":"servicesdesk","i18n":"i18n/i18n.properties","crossNavigation":{"inbounds":{"servicesdesk.tipossolicitud-display":{"semanticObject":"servicesdesk.tipossolicitud","action":"display","signature":{"parameters":{},"additionalParameters":"allowed"}}}}},"sap.cloud":{"public":true,"service":"servicesdesk.service"}}'
+	"servicesdesk/manifest.json":'{"_version":"1.49.0","sap.app":{"id":"servicesdesk","applicationVersion":{"version":"1.0.0"},"type":"application","title":"","description":"servicesdesk","i18n":"i18n/i18n.properties","crossNavigation":{"inbounds":{"servicesdesk.tipossolicitud-display":{"semanticObject":"servicesdesk_tipossolicitud","action":"display","signature":{"parameters":{},"additionalParameters":"allowed"}}}}},"sap.cloud":{"public":true,"service":"servicesdesk.service"}}'
 });
 //# sourceMappingURL=Component-preload.js.map
